07d4f3faed7442b9bbf0fa1e3cfa2e1f497e6542
RafaelFernandesBR <51720927+RafaelFernandesBR@users.noreply.github.com>
2020-04-21 15:59:38 -0300

    Arquivo principal

---
+++ bot.php
@@ -0,0 +1,14 @@
+﻿<?php
+$chat_id="";//seu chat id
+$token="";//A chave API de seu bot
+$name= $_POST["campo2"];
+$mail= $_POST["mail"];
+$tel= $_POST["tel"];
+$final= $_POST["vai"];
+
+$resumo= "Nova mensagem enviada por: $name, email: $mail, Número de telefone: $tel. Mensagem: $final";
+$msg= $resumo;
+$url = "https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text=".$msg."";
+$execucao = file_get_contents($url);
+?>
+
